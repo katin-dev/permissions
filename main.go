ceba2262287edc238199126e963fa0a60bec9a67
katin-dev <katin.dev@gmail.com>
2020-04-04 18:36:38 +0300

    Забираю permissions из БД по user_id

--- main.go
+++ main.go
@@ -2,23 +2,64 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
-
-	"github.com/gorilla/mux"
 )
 
 /*
  Задача: принять HTTP запрос с Auth Token, отдать ошибку авторизации или пермишены пользователя
   1. [x] Поднять HTTP сервер
   2. [x] Написать обработчик: считывает из запроса UserID, возвращает список строк в виде JSON ответа
-  3. Подключить БД и написать запрос для получения permissions
+  3. [x] Подключить БД и написать запрос для получения permissions
+	* Подключить библиотеку
+	* написать SQL, чтоб получить пермишены пользователя
   4. [x] Написать middleware для преобразования токена в user_id
 */
+
+var db *sql.DB
+
+func init() {
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbType := os.Getenv("DB_TYPE")
+	dbUrl := os.Getenv("DB_URL")
+
+	db, err = sql.Open(dbType, dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.Use(CheckAuth)
+	router.HandleFunc("/api/v1/user/permissions", permissionHandler)
+
+	err := http.ListenAndServe(":8085", router)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+}
 
 var CheckAuth = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -38,50 +79,32 @@ var CheckAuth = func(next http.Handler) http.Handler {
 		tokenHeader := r.Header.Get("Authorization") //Получение токена
 
 		if tokenHeader == "" { //Токен отсутствует, возвращаем  403 http-код Unauthorized
-			// response = u.Message(false, "Missing auth token")
 			w.WriteHeader(http.StatusForbidden)
 			w.Header().Add("Content-Type", "application/json")
 			w.Write([]byte("require authorization"))
 			return
 		}
 
-		splitted := strings.Split(tokenHeader, " ") //Токен обычно поставляется в формате `Bearer {token-body}`, мы проверяем, соответствует ли полученный токен этому требованию
+		splitted := strings.Split(tokenHeader, " ")
 		if len(splitted) != 2 {
-			// response = u.Message(false, "Invalid/Malformed auth token")
 			w.WriteHeader(http.StatusForbidden)
 			w.Header().Add("Content-Type", "application/json")
 			w.Write([]byte("invalid access token"))
 			return
 		}
 
-		tokenPart := splitted[1] //Получаем вторую часть токена
+		accessToken := splitted[1]
 
-		fmt.Sprintf("Token %s", tokenPart)
+		fmt.Println(accessToken)
 
-		// Всё прошло хорошо, продолжаем выполнение запроса
-		resp, err := http.PostForm(
-			"http://127.0.0.1:4445/oauth2/introspect",
-			url.Values{"token": {tokenPart}, "scope": {""}},
-		)
+		userId, err := getUserIdByAccessToken(accessToken)
 		if err != nil {
-			fmt.Println(err.Error())
+			log.Fatal(err)
+			http.Error(w, err.Error(), 500)
 			return
 		}
 
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-
-		token := make(map[string]string)
-		err = json.Unmarshal(body, &token)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-
-		fmt.Println(token["sub"])
-
-		userId := token["sub"]
-
-		fmt.Sprintf("User %s", userId) //Полезно для мониторинга
+		fmt.Println(userId)
 
 		ctx := context.WithValue(r.Context(), "user_id", userId)
 		r = r.WithContext(ctx)
@@ -89,17 +112,74 @@ var CheckAuth = func(next http.Handler) http.Handler {
 	})
 }
 
+func getUserIdByAccessToken(accessToken string) (string, error) {
+	hydraUrl := "http://127.0.0.1:4445/oauth2/introspect"
+	resp, err := http.PostForm(
+		hydraUrl,
+		url.Values{"token": {accessToken}, "scope": {""}},
+	)
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Read error: %s", err.Error())
+		return "", err
+	}
+
+	token := make(map[string]interface{})
+	err = json.Unmarshal(body, &token)
+	if err != nil {
+		log.Printf("Json error: %s", err.Error())
+		return "", err
+	}
+
+	return token["sub"].(string), nil
+}
+
+func getUserPermissions(userId string) ([]string, error) {
+	rows, err := db.Query(`
+		SELECT p.name 
+		FROM permission p
+		JOIN role_permission rp ON rp.permission_id = p.id
+		JOIN user_role ur ON ur.role_id = rp.role_id
+		WHERE ur.user_id = $1 	
+	`, userId)
+
+	if err != nil {
+		log.Println("DB error: " + err.Error())
+	}
+
+	defer rows.Close()
+
+	permissions := make([]string, 0)
+
+	for rows.Next() {
+		permission := ""
+		rows.Scan(&permission)
+		permissions = append(permissions, permission)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, error(err)
+	}
+
+	return permissions, nil
+}
+
 func permissionHandler(w http.ResponseWriter, r *http.Request) {
-	// vars := mux.Vars(r)
 	w.Header().Add("Content-Type", "application/json")
 
-	// userId := "uuid"
-	permissions := make([]string, 0)
-	fmt.Println(r.Context().Value("user_id"))
+	userId := r.Context().Value("user_id").(string)
 
-	if r.Context().Value("user_id") == "518f2641-ba9a-42d0-82e2-95ef33831d85" {
-		sergeyPermissions := []string{"read", "dance", "play"}
-		permissions = append(permissions, sergeyPermissions...)
+	permissions, err := getUserPermissions(userId)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
 	}
 
 	json, err := json.Marshal(permissions)
@@ -108,16 +188,4 @@ func permissionHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte(json))
-}
-
-func main() {
-	router := mux.NewRouter()
-	router.Use(CheckAuth)
-	router.HandleFunc("/api/v1/user/permissions", permissionHandler)
-
-	err := http.ListenAndServe(":8085", router)
-
-	if err != nil {
-		fmt.Print(err)
-	}
 }
